46bd603e5b24fa4eb40c13f1e29a23087ea37d79
jason <layjy006@mymail.unisa.edu.au>
2020-09-04 01:09:48 +0930

    -updated views folder -updated controller classes

---
+++ AFCSTEM/Controllers/Unit4Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AFCSTEM.Controllers
+{
+    public class Unit4Controller : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Activity15()
+        {
+            return View();
+        }
+        public IActionResult Activity16()
+        {
+            return View();
+        }
+        public IActionResult Activity17()
+        {
+            return View();
+        }
+    }
+}
